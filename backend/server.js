811bf1acef932d449e5fa682d81c243bd6a4d6a5
usmonalixon <usmonalixonnizomov@gmail.com>
2020-11-19 14:39:42 +0500

    enable coding linting

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import data from "./data.js";
-
+import router from "express";
 const app = express();
 
 app.get("/api/products", (req, res) => {
@@ -9,7 +9,9 @@ app.get("/api/products", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
