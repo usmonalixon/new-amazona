8d51470b7d103936967855bc45444484555f89f0
usmonalixon <usmonalixonnizomov@gmail.com>
2020-11-20 17:48:00 +0500

    adding redux to productScreen

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from "express";
 import data from "./data.js";
-import router from "express";
+
 const app = express();
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product nnot find" });
+  }
+  res.send(data.products);
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
