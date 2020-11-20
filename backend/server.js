1cb0199a0378071ebb02273da9d9d684d628e3ee
usmonalixon <usmonalixonnizomov@gmail.com>
2020-11-20 19:19:07 +0500

    adding api to server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.get("/api/products/:id", (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product nnot find" });
+    res.status(404).send({ message: "Product not found" });
   }
   res.send(data.products);
 });
