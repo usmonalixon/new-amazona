ef72c583a30dd42f3171c8212846c4ce8eac7e22
usmonalixon <usmonalixonnizomov@gmail.com>
2020-11-21 14:24:31 +0500

    signin api

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+dotenv.config();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/newamazona", {
   useNewUrlParser: true,
   useCreateIndex: true,
