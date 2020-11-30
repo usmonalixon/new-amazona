09c56f7eccd8887275a754243bd49766353f3173
usmonalixon <usmonalixonnizomov@gmail.com>
2020-11-30 20:29:04 +0500

    s

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-underscore-dangle */
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -8,19 +6,18 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
-const app = express();
 
+dotenv.config();
+
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-dotenv.config();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/newamazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
-
-const __dirname = path.resolve();
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -28,19 +25,21 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
