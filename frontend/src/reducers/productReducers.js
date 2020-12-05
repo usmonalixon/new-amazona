06e00f83b9ee9adf2cc8e6fbda11be4104c246a3
usmonalixon <usmonalixonnizomov@gmail.com>
2020-12-05 10:23:49 +0500

    Add Category Sidebar and Filter

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-const {
+import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
@@ -17,7 +17,10 @@ const {
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_RESET,
-} = require("../constants/productConstants");
+  PRODUCT_CATEGORY_LIST_REQUEST,
+  PRODUCT_CATEGORY_LIST_SUCCESS,
+  PRODUCT_CATEGORY_LIST_FAIL,
+} from '../constants/productConstants';
 
 export const productListReducer = (
   state = { loading: true, products: [] },
@@ -29,6 +32,22 @@ export const productListReducer = (
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productCategoryListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_CATEGORY_LIST_REQUEST:
+      return { loading: true };
+    case PRODUCT_CATEGORY_LIST_SUCCESS:
+      return { loading: false, categories: action.payload };
+    case PRODUCT_CATEGORY_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
